b15cd4b28cad9916605ab123269d46ebd055ab46
Caterina <caterinasworld@users.noreply.github.com>
2021-04-21 09:16:50 -0700

    Update 04-dom.js

--- javascript/exercises/04-dom.js
+++ javascript/exercises/04-dom.js
@@ -2,22 +2,6 @@
 
 // fetch JavaScript objects representing specific elements in the DOM
 
-let userInput = document.querySelector('#input');
-let copy = document.querySelector('#copy');
-
 // add an event listener on the target element
 
-copy.addEventListener('click', handleClick);
-
 // add a callback function to handle event
-
-function handleClick(event) {
-  console.log(userInput.value);
-  console.log(copy.value);
-
-  let element = document.createElement('div');
-
-  element.textContent = userInput.value;
-
-  document.querySelector('main').append(element);
-}
