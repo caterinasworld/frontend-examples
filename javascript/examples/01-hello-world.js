4a6c2b96bd9e82226ba330737a68e3eb6137377a
Caterina Paun <caterina.paun@gmail.com>
2022-09-26 18:03:00 -0700

    Update: renamed files

--- javascript/examples/01-hello-world.js
+++
@@ -1,6 +0,0 @@
-// JavaScript 01 - Hello World
-
-console.log('hello there');
-
-const today = new Date();
-console.log(`Today's date is ${today}`);
--- jquery/examples/01-hello-world.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-  $('#hello').text('Hello There!');
-  $('#input').on('click', () => {
-    console.log('Hello World, this is jQuery!');
-  });
-});
